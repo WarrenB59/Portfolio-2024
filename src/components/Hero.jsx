9b65de34b07a54d5f8b07865ef07a81304dc3f67
WarrenB59 <bwarren90@hotmail.fr>
2025-01-07 15:30:02 +0100

    Hero modified on surname

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase"
               >
-                Berrahi
+                B.
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
